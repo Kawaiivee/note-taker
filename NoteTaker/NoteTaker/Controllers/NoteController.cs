8325ca6652e656355f4d253d3bc642d39793e93e
Kawaiivee <Kawaiivee@gmail.com>
2023-09-28 12:17:54 -0500

    Still room for guaranteeing uniquity in here -- but that's okay. refactoring

--- NoteTaker/NoteTaker/Controllers/NoteController.cs
+++ NoteTaker/NoteTaker/Controllers/NoteController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using NoteTaker.Models.Entities;
 
@@ -7,27 +8,66 @@ namespace NoteTaker.Controllers
     [Route("[controller]")]
     public class NoteController : ControllerBase
     {
-        private readonly ILogger<NoteController> _logger;
-        public static IEnumerable<Note> notes = Enumerable.Empty<Note>();
+    private readonly ILogger<NoteController> _logger;
+    private static IEnumerable<Note> notes = new List<Note>{
+        new Note
+        {
+            Author = new Author { Id = Guid.NewGuid(), Name = "Kawaiivee" },
+            Title = "Title",
+            Text = "Text",
+        }
+    };
 
         public NoteController(ILogger<NoteController> logger)
         {
             _logger = logger;
-            notes = new List<Note>{
-                new Note
-                {
-                    Author = new Author { Id = Guid.NewGuid(), Name = "Kawaiivee" },
-                    Title = "Title",
-                    Text = "Text",
-                }
-            };
         }
 
         [HttpGet]
         [Route("getNotes")]
-        public IEnumerable<Note> GetNotes()
+        public IActionResult GetNotes()
         {
-            return notes;
+            return Ok(notes);
+        }
+
+        [HttpGet]
+        [Route("getNotesByAuthorId")]
+        public IActionResult GetNotes(string id)
+        {
+            return Ok(notes.Where(note => note?.Author?.Id.ToString() == id));
+        }
+
+
+        [HttpPost]
+        [Route("addNote")]
+        public IActionResult AddNote(Note note)
+        {
+            notes = notes.Append(note);
+            return Created(note.ToString(), note);
+        }
+
+        [HttpPut]
+        [Route("editNote")]
+        public IActionResult UpdateNote(Note note)
+        {
+            if(
+                !(notes?.Any(x => x?.Id == note?.Id)) ??
+                false //for now as a static variable, notes will always be populated
+            )
+            {
+                return BadRequest($"Note with note id: {note?.Id} doesn't exist");
+            }
+            notes = notes.Where(x => !(x?.Id.ToString() == note?.Id.ToString()));
+            notes = notes.Append(note);
+            return Ok(note);
+        }
+
+        [HttpDelete]
+        [Route("deleteNoteByNoteId")]
+        public IActionResult DeleteNote(string id)
+        {
+            notes = notes.Where(note => !(note?.Id.ToString() == id));
+            return Ok();
         }
     }
 }
--- NoteTaker/NoteTaker/Models/Entities/Note.cs
+++ NoteTaker/NoteTaker/Models/Entities/Note.cs
@@ -2,9 +2,9 @@ namespace NoteTaker.Models.Entities
 {
     public class Note
     {
+        public Guid Id { get; set; }
         public Author Author { get; set; }
         public string Title { get; set; } = string.Empty;
-
         public string Text { get; set; } = string.Empty;
     }
 }
