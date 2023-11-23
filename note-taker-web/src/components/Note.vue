637d4870a1c900f8b23b09cac678b4fef6bb9735
Kawaiivee <Kawaiivee@gmail.com>
2023-11-23 11:13:14 -0600

    fixed vue suspense async get

--- note-taker-web/src/components/Note.vue
+++ note-taker-web/src/components/Note.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { defineProps, onMounted } from 'vue';
 
-// import type { NoteModel } from '@/classes/types';
-
-// export interface NoteProps {
-//   note: NoteModel;
-// };
-
-const props = defineProps();
+const props = defineProps(['note']);
 
 onMounted(() => {
   console.log(props);
 });
+
+const handleDeleteClicked = (event: MouseEvent) => {
+  
+};
+
 </script>
 
 <template>
   <v-container>
-    <v-card> ton
-      <!-- <v-card-title>
+    <v-card>
+      <v-card-title>
         {{ `${props.note.title}: ${props.note.text}` }} 
       </v-card-title>
       <v-card-subtitle>
@@ -25,7 +24,7 @@ onMounted(() => {
       </v-card-subtitle>
       <v-card-text>
         {{ `${props.note.text}` }}
-      </v-card-text> -->
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
--- note-taker-web/src/components/Notes.vue
+++ note-taker-web/src/components/Notes.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
+
 import { getNotes } from '@/api/noteTakerApi';
 import type { NoteModel } from '@/classes/types';
 import { ref, onMounted } from 'vue';
 import Note from '../components/Note.vue';
 
-const notes: NoteModel[] = await getNotes();
+const notes = ref<NoteModel[]>([]);
+
+onMounted(async () => {
+  notes.value = await getNotes();
+});
 </script>
 
 <template>
-    <v-container>
-      <v-row v-for="note in notes">
-        <Note
-          :key="note.id"
-          :note="note"
-        />
-      </v-row>
-    </v-container>
+  <v-container>
+    <v-row v-for="note in notes">
+      <Note
+        :key="note.id"
+        :note="note"
+      />
+    </v-row>
+  </v-container>
 </template>
