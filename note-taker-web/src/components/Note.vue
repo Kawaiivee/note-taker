98b53135c8c026d3329fc1aaf2825059506ca4a6
Kawaiivee <Kawaiivee@gmail.com>
2023-12-07 23:45:55 -0600

    add update note

--- note-taker-web/src/components/Note.vue
+++ note-taker-web/src/components/Note.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import { updateNote } from '@/api/noteTakerApi';
 import type { NoteModel } from '@/classes/types';
 import { defineProps, onMounted, ref } from 'vue';
 
@@ -12,8 +13,9 @@ onMounted(() => {
   console.log(props);
 });
 
-const handleUpdateClicked = () => {
-  const newNote = 
+const handleUpdateClicked = async () => {
+  await updateNote(editNote.value);
+  isEditing.value = false;
 };
 
 </script>
@@ -33,26 +35,39 @@ const handleUpdateClicked = () => {
           </v-card-text>
       </v-container>
       <v-container v-else>
-        <v-text-field
-          type="input"
-          label="Note Title"
-          v-model="editNote.title"
-          variant="outlined"
-          required
-        />
-        <v-textarea
-          type="input"
-          label="Note Text"
-          v-model="editNote.text"
-          variant="outlined"
-          required
-        />
-        <v-text-field
-          type="input"
-          label="Author"
-          v-model="editNote.author.name"
-          required
-        />
+        <v-row>
+          <v-text-field
+            type="input"
+            label="Note Title"
+            v-model="editNote.title"
+            variant="outlined"
+            required
+          />
+          <v-textarea
+            type="input"
+            label="Note Text"
+            v-model="editNote.text"
+            variant="outlined"
+            required
+          />
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              type="input"
+              label="Author"
+              v-model="editNote.author.name"
+              required
+            />
+          </v-col>
+          <v-col>
+            <v-btn
+              @click=handleUpdateClicked
+            >
+            Save Note
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-container>
       <v-card-action>
         <v-icon @click.prevent="() => isEditing = !isEditing" icon="mdi-pencil"></v-icon>
