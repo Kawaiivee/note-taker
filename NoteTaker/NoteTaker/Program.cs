e8d8574e1b222c5d4595d4bbbcd1d3ae30c150c7
Kawaiivee <Kawaiivee@gmail.com>
2024-01-06 21:51:57 -0600

    smol changes

--- NoteTaker/NoteTaker/Program.cs
+++ NoteTaker/NoteTaker/Program.cs
@@ -38,7 +38,7 @@ namespace NoteTaker
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My service");
-                c.RoutePrefix = string.Empty;  // Set Swagger UI at apps root
+                c.RoutePrefix = string.Empty;
             });
 
             //app.UseHttpsRedirection();
